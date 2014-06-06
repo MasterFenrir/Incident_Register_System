7cc7fa52a0e19c583731d883d4a7e2437bcc4b87
David Sherman <thedcsherman@gmail.com>
2014-06-06 13:02:50 +0200

    Test stuff

---
+++ android_portal.php
@@ -0,0 +1,47 @@
+<?php
+$databasehost = "localhost";
+$databasename = "helpdesk";
+$databaseusername ="helpdesk";
+$databasepassword = "Sku53_u3";
+
+$con = mysqli_connect($databasehost,$databaseusername,$databasepassword,$databasename) or die('Unable to connect');
+
+$user = $_POST['user'];
+$pw = $_POST['pw'];
+
+$sql = "SELECT * FROM users WHERE username = '$user' AND password = '$pw'";
+$login = mysqli_query($con, $sql) or die('Unable to connect');
+
+if(($get = mysqli_fetch_assoc($login)) > 0) {
+
+    switch($_POST['id']) {
+        case "hardware_list" : echo queryToJSON("SELECT * FROM hardware"); break;
+
+        case "software_list" : echo queryToJSON("SELECT * FROM software"); break;
+
+        case "login" : echo 1; break;
+    }
+}
+else {
+    echo 0;
+    die();
+}
+
+
+
+
+
+function queryToJSON($query)
+{
+    global $con;
+    $resultset = mysqli_query($con, $query) or die('Error connecting to database');
+
+    $return = array();
+
+    while($row = mysqli_fetch_assoc($resultset)) {
+        $return[] = $row;
+    }
+
+    return json_encode($return);
+}
+?>
