27b40a8d2ae396d19adc71e624c864df8f328295
David Sherman <thedcsherman@gmail.com>
2014-06-12 13:55:51 +0200

    Table class

---
+++ Button.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: David
+ * Date: 12-6-14
+ * Time: 13:48
+ */
+
+class Button
+{
+    public function __construct($name, $postValue)
+    {
+        echo    "<form action=\"/index.php\" method=\"post\">";
+        echo    "<input type=\"hidden\" name=\"display\" value=".$postValue.">";
+        echo    "<input class=\"nav\" type=\"submit\" value=".ucfirst($name).">";
+        echo    "</form>";
+    }
+} 
