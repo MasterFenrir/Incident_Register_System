6d8f8a05403e6be87154c56967ac196ec671f8f4
David Sherman <thedcsherman@gmail.com>
2014-06-17 11:44:57 +0200

    Table sorting works again

--- incidentbeheer.php
+++ incidentbeheer.php
@@ -13,8 +13,10 @@
 function displayContentIncident($postData)
 {
     switch($postData) {
-        case "displayIncidenten" : new HelpdeskTable("Incidenten", "SELECT * FROM incidenten", "displayIncidenten", null, null, null); break;
-        default : echo "Hello ".ucfirst($_SESSION['user']);
+        case "displayIncidenten" : displayIncidenten($postData); break;
+        case "displayAddIncident" : displayAddIncident(); break;
+        case "displayMeldingen" : displayMeldingen($postData); break;
+        default : echo "Hello ".ucfirst($_SESSION['user']); break;
     }
 }
 
@@ -34,29 +36,59 @@ function displayMenuIncident()
  */
 function processEventIncident($eventID)
 {
-    switch($eventID){
-
+    switch($eventID) {
+        case "editIncident" : editIncident();
+        case "deleteIncident" : deleteIncident();
+        case "addIncident" : addIncident();
     }
 }
 
 /**
  * This function manages the display of all the incidents known
  */
-function displayIncidenten(){
-
+function displayIncidenten($postData){
+    new HelpdeskTable("Incidenten", "SELECT * FROM incidenten", $postData, "displayEditIncident", "deleteIncident", "nummer");
 }
 
 /*
  * This functions displays the form to add an incident
  */
 function displayAddIncident(){
-
+    formHeader();
+    dateField(null,null,null);
+    textField("Aanvang tijd", null);
+    textField("Eind tijd", null);
+    dropDown("Hardware", queryToArray("SELECT id_hardware FROM hardware"), null);
+    textField("Omschrijving", null);
+    textField("Workaround", null);
+    textField("Contact", null);
+    textField("Prioriteit", null);
+    textField("Status", null);
+    formFooter("addIncident");
 }
 
 /**
  * This functions displays all the incidents that have no priority yet.
  */
-function displayMeldingen(){
+function displayMeldingen($postData)
+{
+    new HelpdeskTable("Incidenten", "SELECT * FROM incidenten WHERE prioriteit = NULL OR prioriteit = ''", $postData, "displayEditIncident", "deleteIncident", "nummer");
+}
+
+function deleteIncident()
+{
+    global $con;
+
+    mysqli_query($con, "DELETE FROM incidenten WHERE nummer ='".$_POST['key']."'");
+}
+
+function editIncident()
+{
+
+}
+
+function addIncident()
+{
 
 }
 
